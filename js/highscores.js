6476b41ae8945a74b2cfb67901bf81858450ebea
s-gajula <sgajula1983@gmail.com>
2024-12-08 13:36:06 +0400

    delete all2

--- js/highscores.js
+++
@@ -1,9 +0,0 @@
-const highScoresList = document.getElementById('highScoresList');
-const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
-
-// add some li to our ul:
-highScoresList.innerHTML = highScores
-.map(score => {
-   return `<li class= "high-score">${score.name} - ${score.score}</li>`;
-}).join(""); //getting a srting with all my li
-
